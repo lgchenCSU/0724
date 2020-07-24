00ca0d05d3d7c42a02776da3dfacfcbce442cfd1
ChenLong <2512119987@qq.com>
2020-07-25 00:14:24 +0800

    0714 second

--- app/src/main/java/com/example/a0724/MainActivity.java
+++ app/src/main/java/com/example/a0724/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //0714  01
+        //0714  02
     }
 }
